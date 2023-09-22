e731f1e030c70d538da5a9b1717cd6b21fd3c7c2
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-09-22 20:10:05 +0530

    `added web service `

--- bot.py
+++ bot.py
@@ -63,10 +63,10 @@ class Bot(Client):
                                           """)
         
         #web-response
-       # app = web.AppRunner(await web_server())
-       # await app.setup()
-       # bind_address = "0.0.0.0"
-       # await web.TCPSite(app, bind_address, PORT).start()
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
 
     async def stop(self, *args):
         await super().stop()
